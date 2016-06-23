05c282c35f6ce506c3d2786758e0ac6d5f60934d
bdyetton <bdyetton@gmail.com>
2016-06-22 22:06:35 -0700

    autoPost

--- server/mturkConnect.js
+++ server/mturkConnect.js
@@ -1,4 +1,4 @@
-var mturkApi = require('../node_modules/mturk-api');
+var mturkApi = require('./mturk-api-master/index');
 
 function mturk() {
   var self = this;
--- server/user.js
+++ server/user.js
@@ -1,4 +1,4 @@
-var fs = require('fs');
+//var fs = require('fs');
 var aws_ = require('./aws');
 var aws = new aws_;
 
@@ -6,17 +6,17 @@ function user(){
   var self = this;
   self.aws = new aws_;
 
-  this.loginFromFileSystem = function(userName){
-    self.userList = fs.readdirSync('./server/Data/User/');
-    self.userList = self.userList.filter(function(item){return item.indexOf(".txt") > -1});
-    if (self.userList.some(function(user){
-        userParts = user.split('.');
-        return userParts[0] === userName;
-      })){
-      return self.loadUser(userName);
-    }
-    return false;
-  };
+  //this.loginFromFileSystem = function(userName){
+  //  self.userList = fs.readdirSync('./server/Data/User/');
+  //  self.userList = self.userList.filter(function(item){return item.indexOf(".txt") > -1});
+  //  if (self.userList.some(function(user){
+  //      userParts = user.split('.');
+  //      return userParts[0] === userName;
+  //    })){
+  //    return self.loadUser(userName);
+  //  }
+  //  return false;
+  //};
 
   this.createUser = function(userData,imServ,cb){
     var user = userData;
@@ -37,15 +37,15 @@ function user(){
     }
 
     //Write to local disk
-    if (loc == 'local') {
-      fs.writeFile('./server/Data/User/' + user.userName + '.txt', JSON.stringify(user), function (err) {
-        if (err) {
-          console.log(err);
-          return false;
-        }
-        return true;
-      })
-    }
+    //if (loc == 'local') {
+    //  fs.writeFile('./server/Data/User/' + user.userName + '.txt', JSON.stringify(user), function (err) {
+    //    if (err) {
+    //      console.log(err);
+    //      return false;
+    //    }
+    //    return true;
+    //  })
+    //}
   };
 
   this.loadUser = function(user,cb,loc) {
@@ -60,17 +60,17 @@ function user(){
       }
     }
 
-    if (loc == 'local') {
-      try {
-        var fileData = fs.readFileSync('./server/Data/User/' + user.userName + '.txt');
-        fileData = fileData.toString('utf8');
-        cb(false,JSON.parse(fileData));
-      } catch (err) {
-        console.log('User not found');
-        console.log(err);
-        cb(true,err)
-      }
-    }
+    //if (loc == 'local') {
+    //  try {
+    //    var fileData = fs.readFileSync('./server/Data/User/' + user.userName + '.txt');
+    //    fileData = fileData.toString('utf8');
+    //    cb(false,JSON.parse(fileData));
+    //  } catch (err) {
+    //    console.log('User not found');
+    //    console.log(err);
+    //    cb(true,err)
+    //  }
+    //}
   };
 };
 
